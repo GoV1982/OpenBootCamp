08f7bf0745c6a90b6e1522755bdcdafa0171753f
GoV1982 <dark_rider_82@yahoo.com.ar>
2023-04-04 11:12:18 -0300

    Create Ejercicio3.java

---
+++ Resolucion_de_ejercicios/Java/Tema1/JavaOBC/src/com/Ejercicios/Ejercicio3.java
@@ -0,0 +1,12 @@
+package com.Ejercicios;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        String[] nombres = {"Gabriel", "Karen", "Delfina", "Katarina"};
+        for (int i=0; i < nombres.length; i++){
+            System.out.println("Mi nombre es " + nombres[i]);
+    }
+
+    }
+}
